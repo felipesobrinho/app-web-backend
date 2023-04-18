780c6e4e629b488fb0f0383f4f452be1a3756976
felipesobrinho <felipe.sobrinho.3@outlook.com>
2023-04-18 11:19:36 -0300

    Feat: Adicionado model Veiculo e ApplicationDbContext

---
+++ app-web-backend/Models/Veiculo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_web_backend.Models
+{
+
+    [Table("Veiculos")]
+
+    public class Veiculo
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar um Nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a Placa")]
+        public string Placa { get; set; }
+    }
+}
--- app-web-backend/Program.cs
+++ app-web-backend/Program.cs
@@ -1,3 +1,6 @@
+using app_web_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace app_web_backend
 {
     public class Program
@@ -8,6 +11,11 @@ namespace app_web_backend
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            // Add ApplicationDbContext Connection
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
 
             var app = builder.Build();
 
